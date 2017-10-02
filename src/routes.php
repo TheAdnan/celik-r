09aa83adcb2d114402f6e1302a84a2a1c7b452a3
TheAdnan <adnankicin92@gmail.com>
2017-10-02 13:13:59 +0200

    iupata

--- src/routes.php
+++ src/routes.php
@@ -12,7 +12,10 @@ $app->get('/clanovi', function (Request $request, Response $response, array $arg
     return $this->renderer->render($response, 'clanovi.phtml', $data);
 });
 
-$app->get('/uplata', function (Request $request, Response $response, array $args) {
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/uplata/{id}', function (Request $request, Response $response, array $args) {
+    if(isset($args["id"])){
+        return $this->renderer->render($response, 'uplata.phtml', $args);
+    } else{
+        return $this->renderer->render($response, 'index.phtml', $args);
+    }
 });
